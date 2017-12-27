0fb88ada5f776e81ffdbdcb3d7c56dcfc144e5c1
Amalan Jenicious <amalan.f@shenll.com>
2017-12-27 20:05:38 +0530

    display data,template syntax and lifecycle hooks added

--- src/app/lifecyclehooks/lifecyclehooks.component.ts
+++ src/app/lifecyclehooks/lifecyclehooks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-lifecyclehooks',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LifecyclehooksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit() {
+    console.log("ng on init");
+  }
+
+  back(){
+    this._location.back();
+  }
+
+  ngOnChanges(){
+    console.log("ng on changes");
+  }
+
+  ngDoCheck(){
+    console.log("ng do check");
+  }
+
+  ngAfterContentInit(){
+    console.log("ng after content init");
+  }
+
+  ngAfterContentChecked(){
+    console.log("ng after content checked");
+  }
+
+  ngAfterViewInit(){
+    console.log("ng after view init");
+  }
+
+  ngAfterViewChecked(){
+    console.log("ng after view checked");
+  }
+
+  ngOnDestroy(){
+    console.log("ng on destroy");
   }
 
 }
